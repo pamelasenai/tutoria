6c4923db1d0b60d85f040a24c1ba2dfe7e24e40c
panha <pamela_vs@estudante.sc.senai.br>
2024-03-24 13:24:36 -0300

    Initial commit

---
+++ src/test/java/com/senai/tutoria/TutoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.tutoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
