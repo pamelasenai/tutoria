0149ab47baabd225f825777ef5a94ce03d835d85
panha <pamela_vs@estudante.sc.senai.br>
2024-03-24 14:14:14 -0300

    :sparkles: adiciona entidade tutor

---
+++ src/main/java/com/senai/tutoria/entities/TutorEntity.java
@@ -0,0 +1,19 @@
+package com.senai.tutoria.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tutores")
+public class TutorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String especialidade;
+}
