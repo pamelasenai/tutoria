ad17c0c8c3e583118d5395d6fd8374763e7e7c0e
panha <pamela_vs@estudante.sc.senai.br>
2024-03-24 18:59:38 -0300

    :sparkles: adiciona service de tutor

---
+++ src/main/java/com/senai/tutoria/services/TutorService.java
@@ -0,0 +1,6 @@
+package com.senai.tutoria.services;
+
+import com.senai.tutoria.entities.TutorEntity;
+
+public interface TutorService extends GenericService<TutorEntity> {
+}
---
+++ src/main/java/com/senai/tutoria/services/TutorServiceImpl.java
@@ -0,0 +1,52 @@
+package com.senai.tutoria.services;
+
+import com.senai.tutoria.entities.TutorEntity;
+import com.senai.tutoria.exception.NotFoundException;
+import com.senai.tutoria.repositories.TutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TutorServiceImpl implements TutorService {
+    private final TutorRepository repository;
+
+    @Override
+    public TutorEntity criar(TutorEntity tutor) {
+        if(tutor.getNome() == null || tutor.getNome().isBlank()) {
+            throw new RuntimeException("Nome não pode estar em branco.");
+        }
+        if(tutor.getEspecialidade() == null || tutor.getEspecialidade().isBlank()) {
+            throw new RuntimeException("Especialidade não pode estar em branco.");
+        }
+        tutor.setId(null);
+        return repository.save(tutor);
+    }
+
+    @Override
+    public List<TutorEntity> buscarTodos() {
+        return repository.findAll();
+    }
+
+    @Override
+    public TutorEntity buscarPorId(Long id) {
+        return repository.findById(id).orElseThrow(
+                () -> new NotFoundException("Tutor com id " + id + " não encontrado.")
+        );
+    }
+
+    @Override
+    public TutorEntity alterar(Long id, TutorEntity tutor) {
+        buscarPorId(id);
+        tutor.setId(id);
+        return repository.save(tutor);
+    }
+
+    @Override
+    public void excluir(Long id) {
+        buscarPorId(id);
+        repository.deleteById(id);
+    }
+}
