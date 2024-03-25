29e9dfe1f2158d71ee77a9c9aea53819fdbda055
panha <pamela_vs@estudante.sc.senai.br>
2024-03-24 21:56:17 -0300

    :sparkles: adiciona busca de agendamento por id de aluno e tutor

--- src/main/java/com/senai/tutoria/repositories/AgendaRepository.java
+++ src/main/java/com/senai/tutoria/repositories/AgendaRepository.java
@@ -3,5 +3,9 @@ package com.senai.tutoria.repositories;
 import com.senai.tutoria.entities.AgendaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AgendaRepository extends JpaRepository<AgendaEntity, Long> {
+    List<AgendaEntity> findByAlunoIdOrderByData(Long alunoId);
+    List<AgendaEntity> findByTutorIdOrderByData(Long tutorId);
 }
